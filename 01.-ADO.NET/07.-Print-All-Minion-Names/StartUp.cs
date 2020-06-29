ad4673a82e2dd19ec4e74c3ee61659cc78ceb2cb
unknown <vladi_dodnikov@mail.bg>
2020-06-29 21:55:01 +0300

    Add other problems.

---
+++ 01.-ADO.NET/07.-Print-All-Minion-Names/StartUp.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07._Print_All_Minion_Names
+{
+    public class StartUp
+    {
+        private const string connectionString = @"Data Source=DESKTOP-MQ67QHE;Database=MinionsDB;Integrated Security=True";
+        static void Main()
+        {
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+            
+            string result = ReOrderMinions(sqlConnection);
+
+            Console.WriteLine(result);
+        }
+
+        private static string ReOrderMinions(SqlConnection sqlConnection)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<string> initialOrderOfMinions = new List<string>();
+            List<string> reOrderedMinions = new List<string>();
+
+            sqlConnection.Open();
+
+            string getMinionsQueryText = "SELECT Name FROM Minions";
+            using SqlCommand getAllNamesCommand = new SqlCommand(getMinionsQueryText, sqlConnection);
+            using SqlDataReader rdr = getAllNamesCommand.ExecuteReader();
+
+            if (!rdr.HasRows)
+            {
+                return sb.ToString().TrimEnd();
+            }
+
+            while (rdr.Read())
+            {
+                initialOrderOfMinions.Add((string)rdr["Name"]);
+            }
+
+            while (initialOrderOfMinions.Count > 0)
+            {
+
+                reOrderedMinions.Add(initialOrderOfMinions[0]);
+
+                initialOrderOfMinions.RemoveAt(0);
+
+                if (initialOrderOfMinions.Count > 0)
+                {
+                    reOrderedMinions.Add(initialOrderOfMinions[initialOrderOfMinions.Count - 1]);
+
+                    initialOrderOfMinions.RemoveAt(initialOrderOfMinions.Count - 1);
+                }
+            }
+
+            foreach (var minion in reOrderedMinions)
+            {
+               sb.AppendLine(minion);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 01.-ADO.NET/08.-Increase-Minion-Age/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace _08._Increase_Minion_Age
+{
+    public class StartUp
+    {
+        private const string connectionString = @"Data Source=DESKTOP-MQ67QHE;Database=MinionsDB;Integrated Security=True";
+        static void Main()
+        {
+            using SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            int[] inputMinionIds = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            string result = IncreaseMinionsAge(sqlConnection, inputMinionIds);
+
+            Console.WriteLine(result);
+        }
+
+        private static string IncreaseMinionsAge(SqlConnection sqlConnection, int[] inputMinionIds)
+        {
+            sqlConnection.Open();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < inputMinionIds.Length; i++)
+            {
+                string updateMinionsQueryText = $"UPDATE Minions SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)), Age += 1 WHERE Id = @MinionId";
+                SqlCommand updateMinionsCommand = new SqlCommand(updateMinionsQueryText, sqlConnection);
+                updateMinionsCommand.Parameters.AddWithValue("@MinionId", inputMinionIds[i]);
+            }
+
+            string getNewMinionsQueryText = $"SELECT Name, Age FROM Minions";
+            using SqlCommand getNewMinionsCommand = new SqlCommand(getNewMinionsQueryText, sqlConnection);
+            
+            using SqlDataReader rdr = getNewMinionsCommand.ExecuteReader();
+            while (rdr.Read())
+            {
+                sb.AppendLine($" {rdr["Name"]} {rdr["Age"]}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
